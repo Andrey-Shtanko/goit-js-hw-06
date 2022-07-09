29efa71621bea7d889c5194158c94ca56766e3d9
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-07-09 09:13:35 +0300

    task-02-done

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector(`#ingredients`);
+
+const arrLi = [];
+
+for (const ingridient of ingredients) {
+  const itemEl = document.createElement(`li`);
+  itemEl.classList.add(`item`);
+  itemEl.textContent = ingridient;
+
+  arrLi.push(itemEl);
+}
+
+list.append(...arrLi);
