5ae3643c8511ed6f180df56edbbad719aa4be027
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-11 16:45:34 +0300

    task-09-done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector(`body`)
+const colorRef = document.querySelector(`.color`)
+const colorBtn = document.querySelector(`.change-color`)
+colorBtn.addEventListener(`click`, () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorRef.textContent = body.style.backgroundColor;
+})
