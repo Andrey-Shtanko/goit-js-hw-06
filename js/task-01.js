126914d16e85f3c0828ce18777044ba384c9dd11
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-14 13:52:28 +0300

    task-01--fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,13 @@
-const ulChildrens = document.querySelector(`#categories`);
-console.log(`Number of categories:${ulChildrens.children.length}`);
+const ulChildrens = document.querySelectorAll(`.item`);
+console.log(`Number of categories: ${ulChildrens.length}`);
 
 console.log(``);
 
-const itemsTitle = document.querySelectorAll(`h2`);
+ulChildrens.forEach(function (item) {
+const itemTitle = item.querySelector(`h2`);
+    const itemUl = item.querySelectorAll(`li`);
+    console.log(`Category: ${itemTitle.textContent}`);
+console.log(`Elements: ${itemUl.length}`);
+console.log(``);
+})
 
-const itemsUl = document.querySelectorAll(`.item>ul`);
-
-console.log(`Category: ${itemsTitle[0].textContent}`);
-console.log(`Elements: ${itemsUl[0].childElementCount}`);
-console.log(``);
-console.log(`Category: ${itemsTitle[1].textContent}`);
-console.log(`Elements: ${itemsUl[1].childElementCount}`);
-console.log(``);
-console.log(`Category: ${itemsTitle[2].textContent}`);
-console.log(`Elements: ${itemsUl[2].childElementCount}`);
-console.log(``);
