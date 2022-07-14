f7e27ff137822c808c869ba76d3818e9524fabd7
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-14 14:44:55 +0300

    task-06-fixed

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -7,10 +7,12 @@ const validRule = Number(inputEl.dataset.length);
 inputEl.addEventListener(`blur`, (event) => {
     if (event.currentTarget.value.length !== validRule) {
         inputEl.classList.add(`invalid`);
+        inputEl.classList.remove(`valid`);
     } else {
-         inputEl.classList.remove(`invalid`);
-    }
+        inputEl.classList.remove(`invalid`);
         inputEl.classList.add(`valid`);
+    }
+        
 })
 
 
