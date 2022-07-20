72f781c0fb25a756a661ff02b3f2f8dafd4234af
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-07-20 10:58:04 +0300

    update-task-07

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,5 +1,9 @@
 const inputValue = document.querySelector(`#font-size-control`);
 const textEl = document.querySelector(`#text`);
+const defaultValue = inputValue.valueAsNumber
+
+textEl.style.fontSize = `${defaultValue}px`
+
 
 inputValue.addEventListener(`input`, (event) => {
      textEl.style.fontSize = `${event.currentTarget.value}px`
